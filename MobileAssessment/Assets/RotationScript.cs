c5610ef99063ac90f483eccb2f8e58534ac15a6d
42silasweis <silasweis18@gmail.com>
2020-02-04 20:11:21 -0800

    Holding space makes the player rotate back and forth

--- MobileAssessment/Assets/RotationScript.cs
+++ MobileAssessment/Assets/RotationScript.cs
@@ -7,29 +7,46 @@ public class RotationScript : MonoBehaviour
     float moveX = 150;
     public float moveSpeed = 1;
     float rotation;
-    bool movingClockWise;
-    bool movingCounterClockWise;
+    bool movingClockWise = false;
+    bool movingCounterClockWise = true;
     float timer;
     float rotationInterval = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        timer += Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
         {
-            RotateLeft();
+            Rotate();
         }
-        rotation = GetComponent<Rigidbody2D>().rotation;
-        
-        GetComponent<Rigidbody2D>().rotation = moveX + moveSpeed * Time.deltaTime;
     }
-    void RotateLeft()
+    void Rotate()
     {
-        transform.Rotate(Vector3.forward * -90);
+        if (movingCounterClockWise && !movingClockWise)
+        {
+            transform.Rotate(Vector3.forward * moveSpeed * Time.deltaTime);
+            //Debug.Log(transform.rotation.z);
+            if (transform.rotation.z >= 0.3420201f && movingCounterClockWise)
+            {
+                movingClockWise = true;
+                movingCounterClockWise = false;
+                //Debug.Log("Booleans change");
+            }
+        }
+        else if (movingClockWise && !movingCounterClockWise)
+        {
+            transform.Rotate(Vector3.forward * -moveSpeed * Time.deltaTime);
+            if (transform.rotation.z <= -0.3420201f && movingClockWise)
+            {
+                movingClockWise = false;
+                movingCounterClockWise = true;
+            }
+        }
     }
 }
