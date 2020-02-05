e2f43b5e8cf4479649104d4cfdc76836a87c7767
Silas Weis <18.silas.weis@ksd.org>
2020-02-05 13:52:04 -0800

    Made level 1 sort of

--- MobileAssessment/Assets/MobileScripts/DifferentJump.cs
+++ MobileAssessment/Assets/MobileScripts/DifferentJump.cs
@@ -4,154 +4,26 @@ using UnityEngine;
 
 public class DifferentJump : MonoBehaviour
 {
-    public float speed = 7;
-    public float jumpForce;
-    private float moveInput;
+    public float speed = 1;
     private Rigidbody2D rb;
-
-    private bool facingRight = true;
-
-    public bool grounded;
-
-    //public bool isGrounded;
-    //public Transform groundCheck;
-    //public float checkRadius;
-    //public LayerMask whatIsGround;
-
-    public int extraJumps;
-    public int extraJumpsValue;
-
-    float initialMoveSpeed;
-    public float sprintMultiplier = 2;
-    float sprintSpeed;
-    public bool sprintKeyDown = false;
-    public bool dir;
-    Animator anim;
-
+    float y;
+    GameObject camera;
+    float bulletspeed;
+    Vector2 velocity;
     void Start()
     {
-        initialMoveSpeed = speed;
-        extraJumps = extraJumpsValue;
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        float y = 1;
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
-    void FixedUpdate()
-    {
-        float x = Input.GetAxisRaw("Horizontal");
-        moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        if (x > 0 && dir == false)
-        {
-            Flip();
-            dir = true;
-        }
-        else if(x < 0 && dir == true)
-        {
-            Flip();
-            dir = false;
-        }
-
-    }
     void Update()
     {
-        if(grounded == true)
+        if (camera == null)
         {
-            extraJumps = extraJumpsValue;
+            camera = GameObject.FindGameObjectWithTag("MainCamera");
         }
-        if (Input.GetButtonDown("Jump") && extraJumps > 0 && grounded == false)
-        {
-            rb.velocity = Vector2.up * jumpForce;
-            extraJumps--;
-        }
-        else if (Input.GetButtonDown("Jump") && grounded == true)
-        {
-            rb.velocity = Vector2.up * jumpForce;
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift) && grounded)
-        {
-            if (sprintKeyDown == false)
-            {
-                sprintKeyDown = true;
-                speed = speed * sprintMultiplier;
-            }
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            speed = initialMoveSpeed;
-            sprintKeyDown = false;
-        }
-        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
-        anim.SetBool("sprint", sprintKeyDown); //make sure to save and surrender
-        anim.SetBool("grounded", grounded);
-        anim.SetFloat("x", velocity.x);
-        anim.SetFloat("y", velocity.y);
-        /*
-        float x = Input.GetAxisRaw("Horizontal");
-        if (x > 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
-        }
-        else if (x < 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
-        }*/
-    }
-
-    void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 Scaler = transform.localScale;
-        Scaler.x *= -1;
-        transform.localScale = Scaler;
-        /*
-        float x = Input.GetAxisRaw("Horizontal");
-        if (x > 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
-        }
-        else if (x < 0)
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
-        }*/
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == 0 && !collision.isTrigger)
-        {
-            grounded = true;
-            extraJumps = extraJumpsValue;
-        }
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == 0 && !collision.isTrigger)
-        {
-            grounded = true;
-            extraJumps = extraJumpsValue;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == 0 && !collision.isTrigger)
-        {
-            grounded = false;
-            //jumpCount = 0;
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "MovingPlatform" || collision.gameObject.tag == "Enemy")
-        {
-            transform.parent = collision.transform;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "MovingPlatform" || collision.gameObject.tag == "Enemy")
-        {
-            this.transform.parent = null;
-        }
+        velocity = camera.GetComponent<Rigidbody2D>().velocity;
+        GetComponent<Rigidbody2D>().velocity = (transform.up * speed);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, velocity.y);
     }
 }
--- MobileAssessment/Assets/RotationScript.cs
+++ MobileAssessment/Assets/RotationScript.cs
@@ -4,49 +4,52 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour
 {
-    float moveX = 150;
-    public float moveSpeed = 1;
-    float rotation;
+    public float moveSpeed = 35;
     bool movingClockWise = false;
     bool movingCounterClockWise = true;
-    float timer;
-    float rotationInterval = 0.2f;
-    // Start is called before the first frame update
+    bool startRotate = false;
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
             Rotate();
         }
+        if (startRotate)
+        {
+            if (movingCounterClockWise && !movingClockWise)
+            {
+                transform.Rotate(Vector3.forward * moveSpeed * Time.deltaTime);
+                //Debug.Log(transform.rotation.z);
+                if (transform.rotation.z >= 0.3420201f && movingCounterClockWise)
+                {
+                    movingClockWise = true;
+                    movingCounterClockWise = false;
+                    //Debug.Log("Booleans change");
+                }
+            }
+            else if (movingClockWise && !movingCounterClockWise)
+            {
+                transform.Rotate(Vector3.forward * -moveSpeed * Time.deltaTime);
+                if (transform.rotation.z <= -0.3420201f && movingClockWise)
+                {
+                    movingClockWise = false;
+                    movingCounterClockWise = true;
+                }
+            }
+        }
     }
-    void Rotate()
+    public void Rotate()
     {
-        if (movingCounterClockWise && !movingClockWise)
-        {
-            transform.Rotate(Vector3.forward * moveSpeed * Time.deltaTime);
-            //Debug.Log(transform.rotation.z);
-            if (transform.rotation.z >= 0.3420201f && movingCounterClockWise)
-            {
-                movingClockWise = true;
-                movingCounterClockWise = false;
-                //Debug.Log("Booleans change");
-            }
-        }
-        else if (movingClockWise && !movingCounterClockWise)
-        {
-            transform.Rotate(Vector3.forward * -moveSpeed * Time.deltaTime);
-            if (transform.rotation.z <= -0.3420201f && movingClockWise)
-            {
-                movingClockWise = false;
-                movingCounterClockWise = true;
-            }
-        }
+        startRotate = true;
+        
+    }
+    public void Stop()
+    {
+        startRotate = false;
     }
 }
